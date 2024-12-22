615f715dfefa8760e40fd591132e5f9e62802396
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-12-22 08:52:00 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAEelXXHhNoWXR_duKpDJiFl39Lb
 CAPTION = os.environ.get("CAPTION", "")
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://INFINITYRENAME24BOT:INFINITYRENAME24BOT@cluster0.colorb9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+ADMIN = int(os.environ.get("ADMIN", '6469754522'))
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
 LOG_CHANNEL = os.environ.get("LOG_CHANNEL_ID", -1002145984196)
