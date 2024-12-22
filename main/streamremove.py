2b852e0e79b42b70e78a1d6f05757659d932142d
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-12-22 07:56:12 +0530

    Update streamremove.py

--- main/streamremove.py
+++ main/streamremove.py
@@ -13,23 +13,18 @@ from pyrogram import Client, filters
 from pyrogram.enums import MessageMediaType
 from pyrogram.errors import MessageNotModified
 from main.utils import progress_message, humanbytes
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup,CallbackQuery
-from config import AUTH_USERS, ADMIN, CAPTION
-from main.utils import heroku_restart, upload_files, download_media, download_file_from_drive
+from config import CAPTION
+from main.utils import upload_files, download_file_from_drive
 import aiohttp
 from pyrogram.errors import RPCError, FloodWait
 import asyncio
-from main.ffmpeg import remove_all_tags, change_video_metadata, generate_sample_video, add_photo_attachment, merge_videos, unzip_file, extract_audio_stream, extract_subtitle_stream, extract_video_stream, extract_audios_from_file, extract_subtitles_from_file, extract_video_from_file, get_mediainfo, compress_video, get_and_upload_mediainfo
 from googleapiclient.http import MediaFileUpload
 from main.gdrive import upload_to_google_drive, extract_id_from_url, copy_file, get_files_in_folder, drive_service
 from googleapiclient.errors import HttpError
-from Database.database import db
 import datetime
 from datetime import timedelta
 import psutil
 from pymongo.errors import PyMongoError
-from yt_dlp import YoutubeDL
-from html_telegraph_poster import TelegraphPoster
 from os import execl as osexecl
 from sys import executable
 from config import *
