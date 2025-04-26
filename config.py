a9b4bb2a1a015c71194306c656afcc7a2c8ff283
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-04-26 14:58:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = os.environ.get("API_ID", "10811400")
 API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6487202001:AAE6LB8v94Ud5C04fBqHSa1IOiiKIzldOQM")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6487202001:AAFgiJfUdrHG7KduMaDYe8nBWpTYQ9h8ISw")
 CAPTION = os.environ.get("CAPTION", "")
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://HARSHA24:HARSHA24@cluster0.sxaj8up.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
