7fc2dad0b34ef86d9892d57c3b363992fe652a0d
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-04-26 22:43:35 +0530

    Update database.py

--- Database/database.py
+++ Database/database.py
@@ -34,7 +34,60 @@ class Database:
     async def clear_database(self):
         # Drop all collections
         await self.users_col.drop()
-                    
+
+
+    async def save_screenshots_count(self, user_id, screenshots_count):
+        await self.users_col.update_one(
+            {'id': user_id},
+            {'$set': {'settings.screenshots_count': screenshots_count}},
+            upsert=True
+        )
+    
+    async def get_screenshots_count(self, user_id):
+        user = await self.users_col.find_one({'id': user_id})
+        if user:
+            return user.get('settings', {}).get('screenshots_count')
+        return None
+    
+    async def get_sample_video_duration(self, user_id):
+        user = await self.users_col.find_one({'id': user_id})
+        if user:
+            return user.get('settings', {}).get('sample_video_duration')
+        return None
+    
+    async def save_thumbnail(self, user_id, file_id):
+        await self.files_col.update_one({'id': user_id}, {'$set': {'thumbnail_file_id': file_id}}, upsert=True)
+        
+    async def get_thumbnail(self, user_id):
+        file_data = await self.files_col.find_one({'id': user_id})
+        if file_data:
+            return file_data.get('thumbnail_file_id')
+        return None
+    
+    async def delete_thumbnail(self, user_id):
+        await self.files_col.update_one({'id': user_id}, {'$unset': {'thumbnail_file_id': ""}})
+
+   async def save_screenshot_paths(self, user_id, screenshot_paths):
+        result = await self.users_col.update_one(
+            {'_id': user_id},
+            {'$set': {'screenshot_paths': screenshot_paths}},
+            upsert=True
+        )
+        return result
+
+    async def get_screenshot_paths(self, user_id):
+        user = await self.users_col.find_one({'_id': user_id})
+        if user:
+            return user.get('screenshot_paths', [])
+        return []
+
+    async def delete_screenshot_paths(self, user_id):
+        result = await self.users_col.update_one(
+            {'_id': user_id},
+            {'$unset': {'screenshot_paths': ''}}
+        )
+        return result.modified_count > 0
+        
 # Initialize the database instance
 db = Database(DATABASE_URI, DATABASE_NAME)    
                   
