5f56e2d73e799cec6d151dbdc0f87e6e9248b066
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-05-04 20:44:34 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

--- config.py
+++ config.py
@@ -17,6 +17,6 @@ ADMIN = int(os.environ.get("ADMIN", '6469754522'))
 SUNRISES_PIC= "https://envs.sh/lHg.jpg"  # Replace with your Telegraph link
 LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002145984196)
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
-PORT = int(os.environ.get("PORT", "8080"))
+PORT = int(os.environ.get("PORT", "8081")) #8080
 
 
