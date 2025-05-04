a031d29f79aba4a77169b0cb486905301606a1ca
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-05-04 19:58:36 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 #SUNRISES24BOTS
 #TG:@SUNRISES_24
 from pyrogram import Client
+from aiohttp import web
+from main.web_support import web_server
 from config import *
 import os
 
@@ -9,7 +11,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name="StreamRemove",
+            name="StreamCleanX",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
@@ -19,9 +21,14 @@ class Bot(Client):
         )
     async def start(self):
         await super().start()
-        me = await self.get_me()      
+        me = await self.get_me()
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()        
         print(f"{me.first_name} | @{me.username} 𝚂𝚃𝙰𝚁𝚃𝙴𝙳...⚡️")
         
+                
         
     async def stop(self, *args):
        await super().stop()      
